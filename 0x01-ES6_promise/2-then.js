32fa67c316ba519e721372bf64a1dd40625e7fa5
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-01 16:02:08 +0300

    catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
-/* eslint-disable no-unused-vars */
 function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success',
-      };
-    })
-    .catch((error) => {
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
     });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 async function handleProfileSignup() {
-  const photo = uploadPhoto();
   const user = createUser();
-  const response1 = await photo;
+  const response1 = await uploadPhoto();
   user.then((response2) => {
     console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
   });
