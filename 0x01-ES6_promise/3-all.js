6bee598f52caa8b6b79494de06177c369bc85d0f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-01 14:08:04 +0300

    3-all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,12 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+async function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  return photo
-    .then((response1) => {
-      user.then((response2) => {
-        console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
-      });
-    });
+  const response1 = await photo;
+  user.then((response2) => {
+    console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
+  });
 }
 
 export default handleProfileSignup;
