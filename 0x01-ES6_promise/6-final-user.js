9684afdd5f41683cc3994867d7fb018efb729cd6
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-01 16:52:54 +0300

    handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  // const user = signUpUser(firstName, lastName);
-  // const photo = uploadPhoto(fileName);
   const respArr = [];
   const obj = { status: '', value: '' };
   try {
