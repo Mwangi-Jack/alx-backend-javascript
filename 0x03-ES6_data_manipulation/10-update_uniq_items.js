78cbe6eedb65a92c8794e85b17c2f2b4c610e9a0
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-03 12:34:58 +0300

    iterating a Map

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+function updateUniqueItems(map1) {
+  if (!(map1 instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  for (const key of map1.keys()) {
+    if (map1.get(key) === 1) {
+      map1.set(key, 100);
+    }
+  }
+  return map1;
+}
+
+export default updateUniqueItems;
