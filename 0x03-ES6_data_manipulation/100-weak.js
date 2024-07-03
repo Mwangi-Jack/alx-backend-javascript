be5d1ba6266c2ff4fe1102c6af85c1f325d3cead
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-03 13:16:09 +0300

    iterating weakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,8 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endPointArg) {
-  const val = weakMap.get(endPointArg);
-  if (!val) {
-    weakMap.set(endPointArg, 1);
-  } else {
-    let count = weakMap.get(endPointArg);
-    if (count >= 5) throw new Error('Endpoint load is high');
-    weakMap.set(endPointArg, count += 1);
-  }
+  let count = weakMap.get(endPointArg) || 0;
+  count += 1;
+  weakMap.set(endPointArg, count);
+  if (count >= 5) throw new Error('Endpoint load is high');
 }
