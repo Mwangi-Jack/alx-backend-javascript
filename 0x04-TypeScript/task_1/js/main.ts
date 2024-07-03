9d91936e13097107a0004a701252f47690760d75
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-03 19:00:35 +0300

    writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// one
 interface Teacher {
 	readonly firstName: string;
 	readonly lastName: string;
@@ -7,6 +8,43 @@ interface Teacher {
 	[key: string]: any;
 }
 
+// two
 interface Directors extends Teacher {
 	numberOfReports: number;
+}
+
+// three
+function printTeacher(firstName: string, lastName: string) {
+	return `${firstName[0]}. ${lastName}`
+}
+
+// four
+
+interface StudentClassInterface {
+	firstName: string;
+	lastName: string;
+	workOnHomework(): string;
+	displayName(): string
+}
+
+interface StudentClassContructor {
+	new (firstName: string, lastName: string): StudentClassInterface
+}
+
+
+class StudentClass implements StudentClassInterface {
+	public firstName: string;
+	public lastName: string;
+
+	constructor(firstName: string, lastName: string) {
+		this.firstName = firstName;
+		this.lastName = lastName
+	}
+	workOnHomework(): string {
+		return "Currently working"
+	}
+
+	displayName(): string {
+		return this.firstName;
+	}
 }
