23741baeb1e8906714f3e6b86abb33920d28deb2
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-15 20:08:18 +0300

    using getters and setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name === 'string') this._name = name;
-    if (typeof length === 'number') this._length = length;
-    if (typeof students === 'object') this._students = students;
+    if (typeof name === 'string') this._name = name; else throw TypeError('Name must be a string');
+    if (typeof length === 'number') this._length = length; else throw TypeError('Length must be a number');
+    if (typeof students === 'object') this._students = students; else throw TypeError('Students must be an array');
   }
 
   set name(name) {
