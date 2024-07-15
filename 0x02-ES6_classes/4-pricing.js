980081624b0ba5ff7c6dbad887788d5540660638
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-15 20:26:35 +0300

    adding a static method to a class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static converPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
