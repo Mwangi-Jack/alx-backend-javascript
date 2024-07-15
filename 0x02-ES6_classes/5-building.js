f22f3d214c06d0ff2ff37e120267b3533ee24eaf
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-15 21:35:04 +0300

    class inheritance

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,13 +2,16 @@
 class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    // if (this.evacuationWarningMessage === undefined) {
-    //   throw new Error('Class extending Building must override evacuationWarningMessage');
-    // }
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage',
+    );
   }
 }
 
