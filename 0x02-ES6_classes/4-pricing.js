f79d5e22e08f3dbd983741c60a62d0ddd7218be1
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-07-15 20:34:57 +0300

    adding a static method to a class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,28 +1,35 @@
+/* eslint-disable no-unused-vars */
+import Currency from './3-currency';
+
 class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  set amount(amount) {
-    this._amount = amount;
-  }
-
   get amount() {
     return this._amount;
   }
 
-  set currency(currency) {
-    this._currency = currency;
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
   }
 
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  static converPrice(amount, conversionRate) {
+  static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
 }
 
-export default Pricing;
+export default Pricing
