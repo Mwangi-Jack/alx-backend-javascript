6e1a3683065b7006cec66546dc7197740cc4d9ba
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-20 18:48:09 +0300

    Created a small HTTP server using Express

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,18 +1,12 @@
 const express = require('express');
 
 const port = 1245;
-const hostname = '127.0.0.1';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  res.statusCode = 200;
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.send('This is the list of our students');
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://${hostname}:${port}`);
-});
+app.listen(port);
