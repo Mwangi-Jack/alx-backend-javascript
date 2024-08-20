7139608e3c8e622b42de61e4e4ff4bfb952523dd
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-20 16:03:51 +0300

    Executing basic javascript with NodeJS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,18 @@
-process.stdin.setEncoding('utf-8');
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data}`);
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+    process.exit();
+  }
 });
 
-process.on('exit', () => {
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
