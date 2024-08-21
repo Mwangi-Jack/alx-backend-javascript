bb2d421c0ed69800b2011e0a1935806edac99270
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-21 17:29:45 +0300

    using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf-8');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  }
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-    process.exit();
-  }
-});
-
-process.stdin.on('end', () => {
   console.log('This important software is now closing');
+  process.exit();
 });
