902c27b6dc1ddee4d735bf2628b20245f8772bf3
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-21 17:34:57 +0300

    created a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,28 @@
 const http = require('http');
-const url = require('url');
-const { countStudents } = require('./3-read_file_async');
+const countStudents = require('./3-read_file_async');
 
-const databaseFile = process.argv[2];
+const port = 1245;
+const path = process.argv[2];
+
 const app = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true).pathname;
-
-  if (reqUrl === '/') {
+  if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!\n');
-  } else if (reqUrl === '/students') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
-    countStudents(databaseFile)
-      .then((data) => {
-        res.end(data);
+
+    countStudents(path)
+      .then(() => {
+        res.end();
       })
       .catch((err) => {
-        res.end(err.message);
+        res.write(err.message);
+        res.end();
       });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
   }
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
-});
+app.listen(port);
 
 module.exports = app;
