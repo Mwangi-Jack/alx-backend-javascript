1b44e82b1245c650755f4505c6062b5184f4ff57
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-21 12:13:21 +0300

    Created a more complex  HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,22 +2,41 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const port = 1245;
-const db = process.argv[2];
+const path = process.argv[2];
+
+// const app = http.createServer((req, res) => {
+//   const { method, url } = req;
+
+//   if (url === '/' && method === 'GET') {
+//     res.statusCode = 200;
+//     res.end('Hello Holberton School!');
+//   } else if (url === '/students' && method === 'GET') {
+//     countStudents(db).then((data) => {
+//       res.statusCode = 200;
+//       res.end(`This is the list of our students\n${data}`);
+//     })
+//       .catch((err) => {
+//         res.statusCode = 404;
+//         res.end(err.message);
+//       });
+//   }
+// });
 
 const app = http.createServer((req, res) => {
-  const { method, url } = req;
-
-  if (url === '/' && method === 'GET') {
-    res.statusCode = 200;
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (url === '/students' && method === 'GET') {
-    countStudents(db).then((data) => {
-      res.statusCode = 200;
-      res.end(`This is the list of our students\n${data}`);
-    })
+  } else if (req.url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+
+    countStudents(path)
+      .then(() => {
+        res.end();
+      })
       .catch((err) => {
-        res.statusCode = 404;
-        res.end(err.message);
+        res.write(err.message);
+        res.end();
       });
   }
 });
