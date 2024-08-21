3fb1f81620fe4e519e9bd71258d02443b50fecfa
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-21 12:18:33 +0300

    Created a more complex  HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,24 +4,6 @@ const countStudents = require('./3-read_file_async');
 const port = 1245;
 const path = process.argv[2];
 
-// const app = http.createServer((req, res) => {
-//   const { method, url } = req;
-
-//   if (url === '/' && method === 'GET') {
-//     res.statusCode = 200;
-//     res.end('Hello Holberton School!');
-//   } else if (url === '/students' && method === 'GET') {
-//     countStudents(db).then((data) => {
-//       res.statusCode = 200;
-//       res.end(`This is the list of our students\n${data}`);
-//     })
-//       .catch((err) => {
-//         res.statusCode = 404;
-//         res.end(err.message);
-//       });
-//   }
-// });
-
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -38,6 +20,9 @@ const app = http.createServer((req, res) => {
         res.write(err.message);
         res.end();
       });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
   }
 });
 
