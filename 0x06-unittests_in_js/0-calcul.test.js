3df1eb88a0bbd7047e78f38be7077ba7073e6f5f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-22 11:44:06 +0300

    Basic test with mocha and Node asertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,23 +3,23 @@ const calculateNumber = require('./0-calcul');
 
 describe('CalculateNumber', function () {
 	it('should round both numbers and return their sum', () => {
-		assert.strictEqual(calculateNumber(4.6, 5.3), 10);
-		assert.strictEqual(calculateNumber(1.4, 3.8), 5);
+		assert.equal(calculateNumber(4.6, 5.3), 10);
+		assert.equal(calculateNumber(1.4, 3.8), 5);
 	  });
 
 	  it('should round negative numbers and return their sum', () => {
-		assert.strictEqual(calculateNumber(-1.4, -3.8), -5);
-		assert.strictEqual(calculateNumber(-1.5, -1.5), -2);
+		assert.equal(calculateNumber(-1.4, -3.8), -5);
+		assert.equal(calculateNumber(-1.5, -1.5), -2);
 	  });
 
 	  it('should round numbers to the nearest integer and handle edge cases', () => {
-		assert.strictEqual(calculateNumber(1.5, 2.5), 5); // both round up
-		assert.strictEqual(calculateNumber(2.1, 3.1), 5); // both round down
-		assert.strictEqual(calculateNumber(0.5, 0.5), 2); // both round up
+		assert.equal(calculateNumber(1.5, 2.5), 5); // both round up
+		assert.equal(calculateNumber(2.1, 3.1), 5); // both round down
+		assert.equal(calculateNumber(0.5, 0.5), 2); // both round up
 	  });
 
 	  it('should handle cases where one number is an integer', () => {
-		assert.strictEqual(calculateNumber(2, 5.6), 8); // only b rounds up
-		assert.strictEqual(calculateNumber(4.2, 3), 7); // only a rounds down
+		assert.equal(calculateNumber(2, 5.6), 8); // only b rounds up
+		assert.equal(calculateNumber(4.2, 3), 7); // only a rounds down
 	  });
 })
