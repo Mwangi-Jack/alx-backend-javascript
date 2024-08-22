700a2d6a7a633dda81081b91c58fff15f989669f
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-22 11:09:49 +0300

    Basic test with mocha and Node asertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,20 +2,20 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('CalculateNumber', function () {
-	it('should return 4', function () {
+	it('', function () {
 		assert.equal(calculateNumber(1, 3), 4)
 	})
 
-	it('should return 5', function () {
+	it('', function () {
 		assert.equal(calculateNumber(1, 3.7), 5)
 	})
-	it('should return 5', function () {
+	it('', function () {
 		assert.equal(calculateNumber(1.2, 3.7), 5)
 	})
-	it('should return 6', function () {
+	it('', function () {
 		assert.equal(calculateNumber(1.5, 3.7), 6)
 	})
-	it('should return NaN', function () {
+	it('', function () {
 		assert.equal(calculateNumber(), NaN)
 	})
 })
