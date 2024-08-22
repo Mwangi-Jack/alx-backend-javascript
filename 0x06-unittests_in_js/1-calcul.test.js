552141b0fb944c6071b01b7114f4c5efd7946330
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-22 12:56:14 +0300

    Combining descriptions

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,21 +3,30 @@ const calculateNumber = require('./1-caclcul');
 
 
 describe('CalculateNumber', function () {
-	it('should round both numbers and return their sum', () => {
-		assert.strictEqual(calculateNumber('SUM', 4.6, 5.3), 10);
-		assert.equal(calculateNumber('SUM', -1.4, -3.8), -5);
-	});
-	it('Should round numbers and subtract', () => {
-		assert.strictEqual(calculateNumber('SUBTRACT', 4.6, 5.3), 0);
-		assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.8), 3)
+	describe('SUM', () => {
+		it('should round both numbers and return their sum', () => {
+			assert.strictEqual(calculateNumber('SUM', 4.6, 5.3), 10);
+			assert.equal(calculateNumber('SUM', -1.4, -3.8), -5);
+		});
 	})
 
-	it('Should round integers and divide them', () => {
-		assert.strictEqual(calculateNumber('DIVIDE', 4.6, 5.3), 1);
-		assert.strictEqual(calculateNumber('DIVIDE', -1.4, -3.8), 0.25);
+	describe('SUBTRACT', () => {
+		it('Should round numbers and subtract', () => {
+			assert.strictEqual(calculateNumber('SUBTRACT', 4.6, 5.3), 0);
+			assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.8), 3)
+		})
 	})
-	it('Should return "Error" if the rounded value of b is zero', () => {
-		assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+
+	describe('DIVIDE', () => {
+		it('Should round integers and divide them', () => {
+			assert.strictEqual(calculateNumber('DIVIDE', 4.6, 5.3), 1);
+			assert.strictEqual(calculateNumber('DIVIDE', -1.4, -3.8), 0.25);
+		})
+
+		it('Should return "Error" if the rounded value of b is zero', () => {
+			assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+		})
 	})
+
 
 })
