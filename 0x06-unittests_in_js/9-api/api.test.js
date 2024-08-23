d03a0d35b58019b438a14ca0fca69bd02a04dfae
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-23 13:55:09 +0300

    regex intergration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,59 @@
+const request = require('request');
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+
+describe('index page', () => {
+	const options = {
+		url: 'http://localhost:7865',
+		method: 'GET',
+	};
+
+	it('check correct status code', () => new Promise((done) => {
+		request(options, (err, res, body) => {
+			expect(res.statusCode).to.equal(200);
+			done();
+		})
+	}));
+
+	it('check correct content', () => new Promise((done) => {
+		request(options, (err, res, body) => {
+			expect(body).to.contain('Welcome to the payment system');
+			done();
+		});
+	}));
+
+	it('check correct content length', () => new Promise((done) => {
+		request(options, (err, res, body) => {
+			expect(res.headers['content-length']).to.equal('29');
+			done();
+		});
+	}));
+});
+
+
+describe('cart page', () => {
+	const options = {
+		url: 'http://localhost:7865/cart/12',
+		method: 'GET'
+	}
+	it('check correct status code for correct url', () => new Promise((done) => {
+		request(options, (err, res, body) => {
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+	}));
+
+	it('check correct content for correct url', () => new Promise((done) => {
+		request(options, (err, res, body) => {
+			expect(body).to.contain('Payment methods for cart 12');
+			done();
+		});
+	}));
+
+	it('check correct status code for incorrect url', () => new Promise((done) => {
+		request.get('http://localhost:7865/NaN', (err, res, body) => {
+			expect(res.statusCode).to.equal(404);
+			done();
+		});
+	}));
+});
