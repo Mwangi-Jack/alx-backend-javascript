6b7392f37ce4e89bb0bdc27ecbdf8867a11ef534
Mwangi-Jack <jasonmuturi2000@gmail.com>
2024-08-23 14:51:38 +0300

    regex intergration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -3,47 +3,41 @@ const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
 describe('index page', () => {
-	const options = {
-		url: 'http://localhost:7865',
-		method: 'GET',
-	};
-
-	it('check correct status code', () => new Promise((done) => {
-		request(options, (err, res, body) => {
-			expect(res.statusCode).to.equal(200);
-			done();
-		})
-	}));
-
-	it('check correct content', () => new Promise((done) => {
-		request(options, (err, res, body) => {
-			expect(body).to.contain('Welcome to the payment system');
-			done();
-		});
-	}));
-
+  const options = {
+    url: 'http://localhost:7865/',
+    method: 'GET',
+  };
+  it('check correct status code', () => new Promise((done) => {
+    request(options, (err, res, body) => {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+    });
+  }));
+  it('check correct content', () => new Promise((done) => {
+    request(options, (err, res, body) => {
+	    expect(body).to.equal('Welcome to the payment system');
+	    done();
+    });
+  }));
 });
 
-
 describe('cart page', () => {
-	it('check correct status code for correct url', () => new Promise((done) => {
-		request.get('http://localhost:7865/cart/12', (err, res, body) => {
-			expect(res.statusCode).to.equal(200);
-			done();
-		});
-	}));
-
-	it('check correct content for correct url', () => new Promise((done) => {
-		request.get('http://localhost:7865/cart/12', (err, res, body) => {
-			expect(body).to.contain('Payment methods for cart 12');
-			done();
-		});
-	}));
-
-	it('check correct status code for incorrect url', () => new Promise((done) => {
-		request.get('http://localhost:7865/NaN', (err, res, body) => {
-			expect(res.statusCode).to.equal(404);
-			done();
-		});
-	}));
+  it('check correct status code for correct url', () => new Promise((done) => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+    });
+  }));
+  it('check correct content for correct url', () => new Promise((done) => {
+    request.get('http://localhost:7865/cart/12', (err, res, body) => {
+	    expect(body).to.contain('Payment methods for cart 12');
+	    done();
+    });
+  }));
+  it('check correct status code for incorrect url', () => new Promise((done) => {
+    request.get('http://localhost:7865/cart/hello', (err, res, body) => {
+	    expect(res.statusCode).to.equal(404);
+	    done();
+    });
+  }));
 });
